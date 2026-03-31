de378bb49149718f4b1e6006326c5d35d98a75f1
Doanh <nguyenngocdoanh.office@gmail.com>
2026-03-31 10:46:05 +0700

    updated

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 
-// Home page - list students
 Route::get('/', [StudentController::class, 'index'])->name('students.index');
 
-// Create student
 Route::get('/students/create', [StudentController::class, 'create'])->name('students.create');
 Route::post('/students', [StudentController::class, 'store'])->name('students.store');
 
-// Edit / Update student
 Route::get('/students/{student}/edit', [StudentController::class, 'edit'])->name('students.edit');
 Route::put('/students/{student}', [StudentController::class, 'update'])->name('students.update');
 
-// Delete student (uses DELETE)
 Route::delete('/students/{student}', [StudentController::class, 'destroy'])->name('students.destroy');
